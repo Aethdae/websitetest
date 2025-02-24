7920f8f935073834a84c4bb923b239d69bb0842b
Aethdae <103084018+Aethdae@users.noreply.github.com>
2025-02-24 22:52:12 +0000

    trytwo

--- app/app-ads.txt/page.tsx
+++ app/app-ads.txt/page.tsx
@@ -5,9 +5,12 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-export default function Home() {
+Amplify.configure(outputs);
+
+export default function Ads() {
   return (
       <a>google.com, pub-8752019656910926, DIRECT, f08c47fec0942fa0</a>
   );
