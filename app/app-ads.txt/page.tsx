dc9503d21d6411ab9f5aed54f9a530546bd1be80
Aethdae <103084018+Aethdae@users.noreply.github.com>
2025-02-24 16:19:54 -0600

    Update page.tsx

--- app/app-ads.txt/page.tsx
+++ app/app-ads.txt/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
 
 export default function Home() {
   return (
